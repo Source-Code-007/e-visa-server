361b87633e28161904f6747d26c35a59221f672f
Source-Code-007 <utsho926@gmail.com>
2024-06-12 22:32:38 +0600

    folder structure changed

--- dist/utils/sendResponse.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sendResponse = (res, statusCode, format) => {
-    res.status(statusCode).send({ success: format === null || format === void 0 ? void 0 : format.success, message: format === null || format === void 0 ? void 0 : format.message, data: (format === null || format === void 0 ? void 0 : format.data) || null });
-};
-exports.default = sendResponse;
